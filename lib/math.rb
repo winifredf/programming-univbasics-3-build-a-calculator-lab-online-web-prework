92d7c2810ea4b34bd13c6aacdea29cf3e3144272
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-29 19:06:51 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,37 @@
 def addition(num1, num2)
+  num1 = 3
+  num2 = 4
+  sum = num1 + num2
 
-end
 
 def subtraction(num1, num2)
+  num1 = 8
+  num2 = 4
+  difference = num1 - num2
 
-end
 
 def division(num1, num2)
+  num1 = 8
+  num2 = 4
+  quotient = num1 / num2
 
-end
+
 
 def multiplication(num1, num2)
+  num1 = 8
+  num2 = 4
+  quotient = num1 * num2
 
-end
 
 def modulo(num1, num2)
+  num1 = 8
+  num2 = 4
+  modulo = num1 % num2
 
-end
 
 def square_root(num)
-
-end
+  num1 = 8
+  num2 = 4
+  sqrt (num1)
+  sqrt (num2)
+  
